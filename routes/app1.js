9792fb1dae8cc0fafdea3a9094e199dd3059e71a
Marek <marek@mail.cz>
2023-03-06 19:42:35 +0100

    predelani aplikace pod app1.js

--- routes/app1.js
+++ routes/app1.js
@@ -1,12 +1,21 @@
 import express from 'express'
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Index na App1")
-})
+import db from "../config/db_conf.js";
 
-router.get('/hello', (req, res) => {
-  res.send('Hello from app 1!');
-});
+router.get("/getphonebook", (req, res) => {
+  const query = `select jmeno, telefon, d.nazev as typ_zarizeni, t.ip_zarizeni 
+  from telephones t 
+  join device_type d on d.id = t.typ_zarizeni
+  order by telefon; `;
+  db.query(query, (err, result, fields) => {
+      if(err) {
+          console.log(err);
+          res.sendStatus(500);
+          return;
+      }
+      res.send(result);
+  })
+})
 
 export default router;
